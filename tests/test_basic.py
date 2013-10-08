8c4913a7c230c738a1ceb2421a539bcd101b30d3
Michael Kocher <mkocher@pacificbiosciences.com>
2013-10-07 16:52:59 -0800

    Bug 22415. Add a placeholder test in pbbarcode for jenkins integration.
    [git-p4: depot-paths = "//depot/software/bioinformatics/tools/pbbarcode/": change = 128226]

---
+++ tests/test_basic.py
@@ -0,0 +1,29 @@
+import logging
+import unittest
+
+log = logging.getLogger(__name__)
+
+
+class TestBasic(unittest.TestCase):
+    def test_01(self):
+        """Place holder so jenkins will generate a coverage report"""
+        self.assertTrue(True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
